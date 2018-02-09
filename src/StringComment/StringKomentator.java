0c6a07f06c8619d58cdc55a56ee1db187f82bd0a
Kulovac Nedim <nedim.kulovac@gmail.com>
2018-02-09 15:07:12 +0100

    uradjena metoda string na neparnim pozicijama i broj uppercase karaktera

--- src/StringComment/StringKomentator.java
+++ src/StringComment/StringKomentator.java
@@ -51,6 +51,15 @@ public class StringKomentator {
 	
 	public static void brojUpercaseKaraktera(String s) {
 		
+		
+		int counter = 0;
+		for (int i = 0; i < s.length(); i++) {
+
+			if (Character.isUpperCase(s.charAt(i))) {
+				counter++;
+			}
+		}
+		System.out.print("\nBroj uppercase karaktera je: " + counter);
 	}
 	
 	public static void brojLowrcaseKaraktera(String s) {
